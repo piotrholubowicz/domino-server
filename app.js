4701fc34b1d70c7909060a8ac74bc406c04994b2
Piotr Hołubowicz <piotrekhol@gmail.com>
2020-04-17 15:42:37 -0700

    Set up cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var gameRouter = require("./routes/game");
@@ -14,6 +15,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+const corsOptions = {
+  exposedHeaders: [
+    "Cache-Control",
+    "Content-Language",
+    "Content-Length",
+    "Content-Type",
+    "Expires",
+    "Last-Modified",
+    "Pragma",
+    "Etag",
+  ],
+};
+app.use(cors(corsOptions));
 
 app.use("/game", gameRouter);
 app.use("/players", playersRouter);
